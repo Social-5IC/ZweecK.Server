e0ad5bf71e2de7b87f863b0d0883fa394ebd15b0
federico.volpato <volpatof142003@gmail.com>
2022-04-05 09:07:39 +0200

    API: /user endpoint completed

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
 
 const dbService = require("./services/db-service");
 const {
-  badParameters,
-  uniqueViolation,
-  internalError,
+    badParameters,
+    uniqueViolation,
+    internalError,
 } = require("./specification/error-cases");
 
 const express = require("express");
@@ -13,32 +13,32 @@ const port = 3000;
 const app = express();
 app.use(express.json());
 
-// ==== POST new user ==================================================================================================
+// ==== LOGIN user ==================================================================================================
 app.post("/auth", async (req, res) => {
-  const queryResult = await dbService.logIn(
-    req.get("mail"),
-    req.get("password")
-  );
+    const queryResult = await dbService.logIn(
+        req.get("mail"),
+        req.get("password")
+    );
 
-  switch (queryResult) {
-    case undefined:
-      res.status(500).json({ error: internalError.description });
-      break;
-    case badParameters.errorCode:
-      res.status(400).json({ error: badParameters.description });
-      break;
-    case uniqueViolation.errorCode:
-      res.status(406).json({ error: uniqueViolation.description });
-      break;
-    default:
-      res.json({
-        token: queryResult,
-      });
-      break;
-  }
+    switch (queryResult) {
+        case undefined:
+            res.status(500).json({error: internalError.description});
+            break;
+        case badParameters.errorCode:
+            res.status(400).json({error: badParameters.description});
+            break;
+        case uniqueViolation.errorCode:
+            res.status(406).json({error: uniqueViolation.description});
+            break;
+        default:
+            res.json({
+                token: queryResult,
+            });
+            break;
+    }
 
-  console.log(
-    `
+    console.log(
+        `
     ==================================================
     ${req.method} ${req.url} HTTP/${req.httpVersion}
     
@@ -48,30 +48,74 @@ app.post("/auth", async (req, res) => {
     Requests body:
     ${req.body}
     ==================================================`
-  );
+    );
 });
+
+// ==== LOGOUT user ==================================================================================================
+app.delete("/auth", async (req, res) => {
+    const queryResult = await dbService.logOut(req.get("token"));
+
+    switch (queryResult) {
+        case undefined:
+            res.status(500).json({error: internalError.description});
+            break;
+        case badParameters.errorCode:
+            res.status(400).json({error: badParameters.description});
+            break;
+        case uniqueViolation.errorCode:
+            res.status(406).json({error: uniqueViolation.description});
+            break;
+        default:
+            res.status(200);
+            break;
+    }
+
+    console.log(
+        `
+    ==================================================
+    ${req.method} ${req.url} HTTP/${req.httpVersion}
+    
+    Headers:
+    ${JSON.stringify(req.headers)}
+    
+    Requests body:
+    ${req.body}
+    ==================================================`
+    );
+});
+
 
 // ==== POST new user ==================================================================================================
-app.delete("/auth", async (req, res) => {
-  const queryResult = await dbService.logOut(req.get("token"));
+app.post("/user", async (req, res) => {
+    const queryResult = await dbService.createUser(
+        req.get("name"),
+        req.get("surname"),
+        req.get("mail"),
+        req.get("password"),
+        req.get("dob"),
+        req.get("sex"),
+        req.get("isAdvertiser")
+    );
 
-  switch (queryResult) {
-    case undefined:
-      res.status(500).json({ error: internalError.description });
-      break;
-    case badParameters.errorCode:
-      res.status(400).json({ error: badParameters.description });
-      break;
-    case uniqueViolation.errorCode:
-      res.status(406).json({ error: uniqueViolation.description });
-      break;
-    default:
-      res.status(200);
-      break;
-  }
+    switch (queryResult) {
+        case undefined:
+            res.status(500).json({error: internalError.description});
+            break;
+        case badParameters.errorCode:
+            res.status(400).json({error: badParameters.description});
+            break;
+        case uniqueViolation.errorCode:
+            res.status(406).json({error: uniqueViolation.description});
+            break;
+        default:
+            res.json({
+                token: queryResult,
+            });
+            break;
+    }
 
-  console.log(
-    `
+    console.log(
+        `
     ==================================================
     ${req.method} ${req.url} HTTP/${req.httpVersion}
     
@@ -81,10 +125,79 @@ app.delete("/auth", async (req, res) => {
     Requests body:
     ${req.body}
     ==================================================`
-  );
+    );
 });
+
+// ==== GET user =======================================================================================================
+app.get("/user", async (req, res) => {
+    const queryResult = await dbService.getUserInfo(req.get("token"));
+
+    switch (queryResult) {
+        case undefined:
+            res.status(500).json({error: internalError.description});
+            break;
+        case badParameters.errorCode:
+            res.status(400).json({error: badParameters.description});
+            break;
+        case uniqueViolation.errorCode:
+            res.status(406).json({error: uniqueViolation.description});
+            break;
+        default:
+            res.json({
+                token: queryResult,
+            });
+            break;
+    }
+
+    console.log(
+        `
+    ==================================================
+    ${req.method} ${req.url} HTTP/${req.httpVersion}
+    
+    Headers:
+    ${JSON.stringify(req.headers)}
+    
+    Requests body:
+    ${req.body}
+    ==================================================`
+    );
+});
+
+// ==== DELETE user ====================================================================================================
+app.delete("/user", async (req, res) => {
+    const queryResult = await dbService.deleteUser(req.get("token"));
+
+    switch (queryResult) {
+        case undefined:
+            res.status(500).json({error: internalError.description});
+            break;
+        case badParameters.errorCode:
+            res.status(400).json({error: badParameters.description});
+            break;
+        case uniqueViolation.errorCode:
+            res.status(406).json({error: uniqueViolation.description});
+            break;
+        default:
+            res.status(200);
+            break;
+    }
+
+    console.log(
+        `
+    ==================================================
+    ${req.method} ${req.url} HTTP/${req.httpVersion}
+    
+    Headers:
+    ${JSON.stringify(req.headers)}
+    
+    Requests body:
+    ${req.body}
+    ==================================================`
+    );
+});
+
 
 // ==== Starting the server ============================================================================================
 app.listen(port, () => {
-  console.log("==== Server running ==================================");
+    console.log("==== Server running ==================================");
 });
